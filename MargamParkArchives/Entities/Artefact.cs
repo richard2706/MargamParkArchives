a869a97fa098d55a9b131df28adc072cd2103d6e
Richard Wakeham <rwakeham27@outlook.com>
2025-01-23 12:24:24 +0000

    Add test project and 1st example test method

---
+++ MargamParkArchives/Entities/Artefact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MargamParkArchives.Entities
+{
+    internal class Artefact
+    {
+        //public required string IdentifierGroup { get; set; }
+        //public required int IdentifierKeyNumber { get; init; }
+
+        internal int TestMethod()
+        {
+            return 0;
+        }
+    }
+}
