3129855995257816b1eca0678a8c14535f7f78a3
Richard Wakeham <rwakeham27@outlook.com>
2025-01-26 15:29:53 +0000

    Create IdentiferGroup with corresponding tests

---
+++ MargamParkArchives/Entities/IdentifierGroup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MargamParkArchives.Entities
+{
+    /// <summary>
+    /// Represents an identifer group entity from the database.
+    /// </summary>
+    /// <param name="Id">3 character code uniquely identifying the identifier group.</param>
+    /// <param name="Name">Name of the identifier group.</param>
+    internal record IdentifierGroup
+    {
+        private const int MaxIdLength = 3;
+        private const string IdEmptyErrorMsg = "The Id cannot be an empty string.";
+        private readonly string IdTooLongErrorMsg = string.Format("The Id cannot be longer than {0} characters", MaxIdLength);
+
+        private readonly string _id;
+        internal string Id
+        {
+            get => _id;
+            init
+            {
+                if (value.Length <= 0)
+                {
+                    throw new ArgumentException(IdEmptyErrorMsg);
+                }
+                else if (value.Length > 3)
+                {
+                    throw new ArgumentException(IdTooLongErrorMsg);
+                }
+                _id = value;
+            }
+        }
+
+        internal string Name { get; init; }
+
+        internal IdentifierGroup(string id, string name)
+        {
+            _id = id;
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ MargamParkArchivesTests/Entities/IdentifierGroupTests.cs
@@ -0,0 +1,43 @@
+namespace MargamParkArchivesTests.Entities;
+
+using MargamParkArchives.Entities;
+using static TestMessageConstants;
+
+[TestClass]
+public class IdentifierGroupTests
+{
+    private const string LongIdNoExeptionMsg = "An id longer than 3 characters was incorrectly accepted when instatiating the IdentiferGroup.";
+    private const string EmptyStringNoExeptionMsg = "An id set as the empty string was incorrectly accepted when instatiating the IdentiferGroup.";
+
+    private readonly string _idMismatchMsg = GetPropertyMismatchMsg(nameof(IdentifierGroup.Id), nameof(IdentifierGroup));
+    private readonly string _nameMismatchMsg = GetPropertyMismatchMsg(nameof(IdentifierGroup.Name), nameof(IdentifierGroup));
+
+    [TestMethod]
+    public void CreateInstance_GivenAllValidData_ContainsAllData()
+    {
+        const string id = "ABC";
+        const string name = "Test";
+        IdentifierGroup identifierGroup = new(id, name);
+
+        Assert.AreEqual(id, identifierGroup.Id, _idMismatchMsg);
+        Assert.AreEqual(name, identifierGroup.Name, _nameMismatchMsg);
+    }
+
+    [TestMethod]
+    [Description("Verifies that an identifer group cannot have a key of more than 3 characters.")]
+    public void CreateInstance_GivenLongId_ThrowsException()
+    {
+        const string id = "ABCD";
+        const string name = "Test";
+        Assert.ThrowsException<ArgumentException>(() => new IdentifierGroup(id, name), LongIdNoExeptionMsg);
+    }
+
+    [TestMethod]
+    [Description("Verifies that an identifer group cannot have the empty string as a valid key.")]
+    public void CreateInstance_GivenEmptyId_ThrowsException()
+    {
+        const string id = "";
+        const string name = "Test";
+        Assert.ThrowsException<ArgumentException>(() => new IdentifierGroup(id, name), EmptyStringNoExeptionMsg);
+    }
+}
