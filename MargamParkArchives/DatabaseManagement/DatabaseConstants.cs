9c2e962a0b077dcecdff36b05bdb40dff5d732b2
Richard Wakeham <rwakeham27@outlook.com>
2025-01-27 01:32:31 +0000

    Create artefact table constants for field names

---
+++ MargamParkArchives/DatabaseManagement/DatabaseConstants.cs
@@ -0,0 +1,33 @@
+﻿namespace MargamParkArchives.DatabaseManagement
+{
+    internal static class DatabaseConstants
+    {
+        // Artefact table
+        internal const string ArtefactTableName = "artefact";
+        internal const string ArtefactIdentiferGroupId = "identifier_group_id";
+        internal const string ArtefactIdentifierNumber = "identifier_number";
+        internal const string ArtefactIdentifierKey = "identifier_key";
+        internal const string ArtefactFilePath = "file_path";
+        internal const string ArtefactDateCreated = "date_created";
+        internal const string ArtefactDateModified = "date_modified";
+        internal const string ArtefactParentId = "parent_id";
+        internal const string ArtefactNotes = "notes";
+        internal const string ArtefactTitleEn = "title_en";
+        internal const string ArtefactTitleCy = "title_cy";
+        internal const string ArtefactDescriptionEn = "Description_en";
+        internal const string ArtefactDescriptionCy = "Description_cy";
+        internal const string ArtefactTagsCy = "tags_cy";
+        internal const string ArtefactCultureTagEn = "culture_tag_en";
+        internal const string ArtefactLocationCoverage = "location_coverage";
+        internal const string ArtefactRightType1 = "right_type_1";
+        internal const string ArtefactRightHolder1En = "right_holder_1_en";
+        internal const string ArtefactRightHolder1Cy = "right_holder_1_cy";
+        internal const string ArtefactVisualArtefact = "visual_artefact";
+
+        internal const string CategoryId = "category_id";
+        internal const string PeriodId = "period_id";
+        internal const string CreatorId = "creator_id";
+        internal const string GeneralLocationId = "general_location_id";
+        internal const string SpecificLocationId = "specific_location_id";
+    }
+}
