4289be38bcd216861b46bc12b350983866436c22
Richard Wakeham <rwakeham27@outlook.com>
2025-02-05 20:12:50 +0000

    Restric database constants access level to library only

--- MargamParkArchivesData/DatabaseConstants.cs
+++ MargamParkArchivesData/DatabaseConstants.cs
@@ -1,33 +1,33 @@
 ﻿namespace MargamParkArchivesData
 {
-    public static class DatabaseConstants
+    internal static class DatabaseConstants
     {
         // Artefact table
-        public const string ArtefactTableName = "artefact";
-        public const string ArtefactIdentiferGroupId = "identifier_group_id";
-        public const string ArtefactIdentifierNumber = "identifier_number";
-        public const string ArtefactIdentifierKey = "identifier_key";
-        public const string ArtefactFilePath = "file_path";
-        public const string ArtefactDateCreated = "date_created";
-        public const string ArtefactDateModified = "date_modified";
-        public const string ArtefactParentId = "parent_id";
-        public const string ArtefactNotes = "notes";
-        public const string ArtefactTitleEn = "title_en";
-        public const string ArtefactTitleCy = "title_cy";
-        public const string ArtefactDescriptionEn = "Description_en";
-        public const string ArtefactDescriptionCy = "Description_cy";
-        public const string ArtefactTagsCy = "tags_cy";
-        public const string ArtefactCultureTagEn = "culture_tag_en";
-        public const string ArtefactLocationCoverage = "location_coverage";
-        public const string ArtefactRightType1 = "right_type_1";
-        public const string ArtefactRightHolder1En = "right_holder_1_en";
-        public const string ArtefactRightHolder1Cy = "right_holder_1_cy";
-        public const string ArtefactVisualArtefact = "visual_artefact";
+        internal const string ArtefactTableName = "artefact";
+        internal const string ArtefactIdentiferGroupId = "identifier_group_id";
+        internal const string ArtefactIdentifierNumber = "identifier_number";
+        internal const string ArtefactIdentifierKey = "identifier_key";
+        internal const string ArtefactFilePath = "file_path";
+        internal const string ArtefactDateCreated = "date_created";
+        internal const string ArtefactDateModified = "date_modified";
+        internal const string ArtefactParentId = "parent_id";
+        internal const string ArtefactNotes = "notes";
+        internal const string ArtefactTitleEn = "title_en";
+        internal const string ArtefactTitleCy = "title_cy";
+        internal const string ArtefactDescriptionEn = "Description_en";
+        internal const string ArtefactDescriptionCy = "Description_cy";
+        internal const string ArtefactTagsCy = "tags_cy";
+        internal const string ArtefactCultureTagEn = "culture_tag_en";
+        internal const string ArtefactLocationCoverage = "location_coverage";
+        internal const string ArtefactRightType1 = "right_type_1";
+        internal const string ArtefactRightHolder1En = "right_holder_1_en";
+        internal const string ArtefactRightHolder1Cy = "right_holder_1_cy";
+        internal const string ArtefactVisualArtefact = "visual_artefact";
 
-        public const string CategoryId = "category_id";
-        public const string PeriodId = "period_id";
-        public const string CreatorId = "creator_id";
-        public const string GeneralLocationId = "general_location_id";
-        public const string SpecificLocationId = "specific_location_id";
+        internal const string CategoryId = "category_id";
+        internal const string PeriodId = "period_id";
+        internal const string CreatorId = "creator_id";
+        internal const string GeneralLocationId = "general_location_id";
+        internal const string SpecificLocationId = "specific_location_id";
     }
 }
