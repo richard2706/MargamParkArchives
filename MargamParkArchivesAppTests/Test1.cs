fb86f39ebb43933d0487ae8e3ac29e37b793001c
Richard Wakeham <rwakeham27@outlook.com>
2025-02-05 20:18:10 +0000

    Create test project for app

---
+++ MargamParkArchivesAppTests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace MargamParkArchivesAppTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
