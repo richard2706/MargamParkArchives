66fccf473b5a7fd205842387deebe543c79efa81
Richard Wakeham <rwakeham27@outlook.com>
2025-12-29 23:27:38 +0000

    Create password missing exception (for use when database password is requested but doesn't exist, so needs to be provided by user)

---
+++ MargamParkArchives.Core/PasswordMissingException.cs
@@ -0,0 +1,16 @@
+﻿namespace MargamParkArchives.Core;
+
+public class PasswordMissingException : Exception
+{
+    public PasswordMissingException() : base()
+    {
+    }
+
+    public PasswordMissingException(string message) : base(message)
+    {
+    }
+
+    public PasswordMissingException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
