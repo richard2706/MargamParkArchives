895c2109957aa3475cdb1904c6c20c48b14aa8b4
Richard Wakeham <rwakeham27@outlook.com>
2025-12-30 23:17:40 +0000

    Implement password validation

---
+++ MargamParkArchives.Data/Connections/DirectPasswordProvider.cs
@@ -0,0 +1,13 @@
+﻿using MargamParkArchives.Core;
+
+namespace MargamParkArchives.Data.Connections;
+
+public class DirectPasswordProvider(string password) : IPasswordProvider
+{
+    private readonly string _password = password;
+
+    public string GetPassword()
+    {
+        return _password;
+    }
+}
--- MargamParkArchives.Data/Connections/MySqlPasswordValidationService.cs
+++ MargamParkArchives.Data/Connections/MySqlPasswordValidationService.cs
@@ -1,16 +1,37 @@
 ﻿using MargamParkArchives.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MySqlConnector;
 
 namespace MargamParkArchives.Data.Connections;
 
-public class MySqlPasswordValidationService : IDatabasePasswordValidationService
+public class MySqlPasswordValidationService(IOptions<DatabaseOptions> databaseOptions) : IDatabasePasswordValidationService
 {
-    public Task<bool> ValidatePasswordAsync(string password)
+    private readonly DatabaseOptions _databaseOptions = databaseOptions.Value;
+
+    public async Task<PasswordValidationResponse> ValidatePasswordAsync(string password)
     {
-        throw new NotImplementedException();
+        DirectPasswordProvider passwordProvider = new(password);
+        MySqlConnectionStringProvider connectionStringProvider = new(databaseOptions, passwordProvider);
+        string connectionString = connectionStringProvider.GetConnectionString();
+        await using MySqlConnection connection = new(connectionString);
+        try
+        {
+            await connection.OpenAsync();
+            return new PasswordValidationResponse(PasswordValidationResult.Correct);
+        }
+        catch (MySqlException ex)
+        {
+            return ex.SqlState switch
+            {
+                // Invalid authorization specification
+                "28000" => new PasswordValidationResponse(PasswordValidationResult.Incorrect, ex),
+
+                // Communication link failure (i.e. server connection issue)
+                "08S01" => new PasswordValidationResponse(PasswordValidationResult.ServerUnreachable, ex),
+
+                // Other exception
+                _ => new PasswordValidationResponse(PasswordValidationResult.OtherError, ex),
+            };
+        }
     }
 }
