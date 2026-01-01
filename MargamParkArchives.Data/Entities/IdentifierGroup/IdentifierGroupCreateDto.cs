558a26dbbad2c148f0ab2b520d0117bf88cdd2e2
Richard Wakeham <rwakeham27@outlook.com>
2026-01-01 17:47:47 +0000

    Create core model, create DTO and update DTO for identifier group entity

---
+++ MargamParkArchives.Data/Entities/IdentifierGroup/IdentifierGroupCreateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MargamParkArchives.Data.Entities.IdentifierGroup;
+
+internal record IdentifierGroupCreateDto
+{
+    internal required string IdentifierGroupId { get; init; }
+    internal required string Name { get; init; }
+}
---
+++ MargamParkArchives.Data/Entities/IdentifierGroup/IdentifierGroupUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MargamParkArchives.Data.Entities.IdentifierGroup;
+
+internal record IdentifierGroupUpdateDto
+{
+    internal required string ExistingIdentifierGroupId { get; init; }
+    internal required string NewIdentifierGroupId { get; init; }
+    internal required string Name { get; init; }
+}
