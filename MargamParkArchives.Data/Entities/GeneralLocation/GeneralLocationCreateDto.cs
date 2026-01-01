cb2f6f6d91b83c0c4676407de9fca7e21ac556da
Richard Wakeham <rwakeham27@outlook.com>
2026-01-01 17:40:29 +0000

    Create core model, create DTO and update DTO for general location entity

---
+++ MargamParkArchives.Data/Entities/GeneralLocation/GeneralLocationCreateDto.cs
@@ -0,0 +1,6 @@
+﻿namespace MargamParkArchives.Data.Entities.GeneralLocation;
+
+internal record GeneralLocationCreateDto
+{
+    internal required string Name { get; init; }
+}
---
+++ MargamParkArchives.Data/Entities/GeneralLocation/GeneralLocationUpdateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MargamParkArchives.Data.Entities.GeneralLocation;
+
+internal record GeneralLocationUpdateDto
+{
+    internal required int GeneralLocationId { get; init; }
+    internal required string Name { get; init; }
+}
