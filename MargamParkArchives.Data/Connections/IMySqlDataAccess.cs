f361d8512f60693ae2f81d8e7f80c183e19c2aa4
Richard Wakeham <rwakeham27@outlook.com>
2026-01-01 13:28:16 +0000

    Create generic data access interface and placeholder implementation

---
+++ MargamParkArchives.Data/Connections/IMySqlDataAccess.cs
@@ -0,0 +1,10 @@
+﻿namespace MargamParkArchives.Data.Connections;
+
+public interface IMySqlDataAccess
+{
+    public Task<IEnumerable<T>> GetManyItemsAsync<T, P>(string sqlQuery, P parameters, string connectionString);
+
+    public Task<T?> GetOneItemAsync<T, P>(string sqlQuery, P parameters, string connectionString);
+
+    // TODO add methods for insert one, insert many, update one, delete
+}
---
+++ MargamParkArchives.Data/Connections/MySqlDataAccess.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MargamParkArchives.Data.Connections;
+
+public class MySqlDataAccess : IMySqlDataAccess
+{
+    /// <summary>
+    /// Gets a collection of items of the specified type T from the database asynchronously.
+    /// </summary>
+    /// <typeparam name="T">The type of the items to be returned which should correspond to the database
+    /// entity.</typeparam>
+    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
+    /// <param name="sqlQuery">The SQL select query to execute. The entity being selected must correspond to the type
+    /// T of items being returned.</param>
+    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
+    /// structure should match the parameters in the query.</param>
+    /// <param name="connectionString">The connection string used to connect to the database.</param>
+    /// <returns>A task that represents the asynchronous operation, which will return a collection of items of type T
+    /// returned by the query.</returns>
+    public async Task<IEnumerable<T>> GetManyItemsAsync<T, P>(string sqlQuery, P parameters, string connectionString)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Gets a single item of the specified type T from the database asynchronously. Will return the first item if the
+    /// query returns multiple items.
+    /// </summary>
+    /// <typeparam name="T">The type of the item to be returned which should correspond to the database
+    /// entity.</typeparam>
+    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
+    /// <param name="sqlQuery">The SQL select query to execute. The entity being selected must correspond to the type
+    /// T of items being returned.</param>
+    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
+    /// structure should match the parameters in the query.</param>
+    /// <param name="connectionString">The connection string used to connect to the database.</param>
+    /// <returns>A task that represents the asynchronous operation, which will return an item of type T
+    /// returned by the query, or the first item if the query returns multiple items.</returns>
+    public async Task<T?> GetOneItemAsync<T, P>(string sqlQuery, P parameters, string connectionString)
+    {
+        throw new NotImplementedException();
+    }
+}
