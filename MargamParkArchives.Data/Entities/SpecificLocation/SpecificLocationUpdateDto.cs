de5614e2f3ca88ff71a78fe834fbccd656b670cf
Richard Wakeham <rwakeham27@outlook.com>
2026-01-01 17:37:18 +0000

    Correct specific location update dto so it is a record

--- MargamParkArchives.Data/Entities/SpecificLocation/SpecificLocationUpdateDto.cs
+++ MargamParkArchives.Data/Entities/SpecificLocation/SpecificLocationUpdateDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace MargamParkArchives.Data.Entities.SpecificLocation;
 
-internal class SpecificLocationUpdateDto
+internal record SpecificLocationUpdateDto
 {
     internal required int SpecificLocationId { get; init; }
     internal required string Summary { get; init; }
