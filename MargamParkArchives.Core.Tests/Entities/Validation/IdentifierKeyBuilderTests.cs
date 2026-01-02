1b8acc4b2f516c9a5379f4a0e04fcf324e1ed222
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 16:16:57 +0000

    Create identifier key reconstruction method for use if identifier key is null (and create core test project)

---
+++ MargamParkArchives.Core.Tests/Entities/Validation/IdentifierKeyBuilderTests.cs
@@ -0,0 +1,18 @@
+using MargamParkArchives.Core.Entities.Validation;
+
+namespace MargamParkArchives.Core.Tests.Entities.Validation
+{
+    public class IdentifierKeyBuilderTests
+    {
+        [Theory]
+        [InlineData("CIS-000001", "CIS", 1)]
+        [InlineData("CIS-201984", "CIS", 201984)]
+        [InlineData("WW2-000001", "WW2", 1)]
+        public void BuildsCorrectIdentiferKey_FromValidIdAndNumber(string expectedIdentifierKey,
+            string identiferGroupId, int identiferNumber)
+        {
+            string actualIdentifierKey = IdentifierKeyBuilder.Build(identiferGroupId, identiferNumber);
+            Assert.Equal(expectedIdentifierKey, actualIdentifierKey);
+        }
+    }
+}
---
+++ MargamParkArchives.Core/Entities/Validation/IdentifierKeyBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace MargamParkArchives.Core.Entities.Validation;
+
+internal static class IdentifierKeyBuilder
+{
+    internal static string Build(string identiferGroupId, int identiferNumber)
+    {
+        return $"{identiferGroupId}-{identiferNumber:D6}";
+    }
+}
