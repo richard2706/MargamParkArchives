ba36628826fd0a168205b2db71c7ec1583006748
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 21:18:53 +0000

    Test artefact details read model

---
+++ MargamParkArchives.Core.Tests/Entities/ArtefactDetailsReadModelTests.cs
@@ -0,0 +1,22 @@
+﻿using MargamParkArchives.Core.Entities.ArtefactDetailsReadModel;
+
+namespace MargamParkArchives.Core.Tests.Entities;
+
+public class ArtefactDetailsReadModelTests
+{
+    // Valid construction
+    [Fact]
+    public void Constructor_WithValidData_CreatesObject()
+    {
+        ArtefactDetailsReadModel actual = new("A", 1, "A-000001", "Apple");
+        Assert.NotNull(actual);
+    }
+
+    [Fact]
+    public void Constructor_MissingIdentifierKey_CreatesObjectWithIdentifierKey()
+    {
+        ArtefactDetailsReadModel actual = new("A", 1, null, "Apple");
+        Assert.NotNull(actual);
+        Assert.Equal("A-000001", actual.IdentifierKey);
+    }
+}
