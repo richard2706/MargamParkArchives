dbda30b6ebacd658b0ec7834dd8214d097cb81d3
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 20:12:10 +0000

    Test identifier group core model

---
+++ MargamParkArchives.Core.Tests/Entities/IdentifierGroupTests.cs
@@ -0,0 +1,39 @@
+﻿using MargamParkArchives.Core.Entities.IdentifierGroupEntity;
+
+namespace MargamParkArchives.Core.Tests.Entities;
+
+public class IdentifierGroupTests
+{
+    // Valid construction
+    [Theory]
+    [InlineData("A", "Apple")] // Min length Id
+    [InlineData("AAA", "Apple")] // Max length Id
+    public void Constructor_WithValidData_CreatesObject(string id, string name)
+    {
+        IdentifierGroup actual = new(id, name);
+        Assert.NotNull(actual);
+    }
+
+    // Invalid construction
+    [Theory]
+    [InlineData("", "Apple")]
+    [InlineData("A", "")]
+    public void Constructor_WithEmptyStringProperty_ThrowsException(string id, string name)
+    {
+        Assert.ThrowsAny<ArgumentException>(() => new IdentifierGroup(id, name));
+    }
+
+    [Fact]
+    public void Constructor_WithTooLongId_ThrowsException()
+    {
+        string tooLongId = new('a', IdentifierGroupRules.IdMaxLength + 1);
+        Assert.ThrowsAny<ArgumentException>(() => new IdentifierGroup(tooLongId, "Apple"));
+    }
+
+    [Fact]
+    public void Constructor_WithTooLongName_ThrowsException()
+    {
+        string tooLongName = new('a', IdentifierGroupRules.NameMaxLength + 1);
+        Assert.ThrowsAny<ArgumentException>(() => new IdentifierGroup("A", tooLongName));
+    }
+}
