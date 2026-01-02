5ebcc79cc9f4f70768b725c82c343a1a95e71fc2
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 11:26:15 +0000

    Validate artefact content properties

--- MargamParkArchives.Core/Entities/ArtefactEntity/ArtefactContent.cs
+++ MargamParkArchives.Core/Entities/ArtefactEntity/ArtefactContent.cs
@@ -7,4 +7,34 @@ public class ArtefactContent
     public string? DescriptionEn { get; }
     public string? DescriptionCy { get; }
     public string? Notes { get; }
+
+    public ArtefactContent(string? titleEn = null, string? titleCy = null, string? descriptionEn = null, string? descriptionCy = null, string? notes = null)
+    {
+        if (!ArtefactContentRules.IsValidTitle(titleEn, nameof(titleEn), out string error))
+        {
+            throw new ArgumentException(error, nameof(titleEn));
+        }
+        else if (!ArtefactContentRules.IsValidTitle(titleCy, nameof(titleCy), out error))
+        {
+            throw new ArgumentException(error, nameof(titleCy));
+        }
+        else if (!ArtefactContentRules.IsValidDescription(descriptionEn, nameof(descriptionEn), out error))
+        {
+            throw new ArgumentException(error, nameof(descriptionEn));
+        }
+        else if (!ArtefactContentRules.IsValidDescription(descriptionCy, nameof(descriptionCy), out error))
+        {
+            throw new ArgumentException(error, nameof(descriptionCy));
+        }
+        else if (!ArtefactContentRules.IsValidNotes(notes, nameof(notes), out error))
+        {
+            throw new ArgumentException(error, nameof(notes));
+        }
+
+        TitleEn = titleEn;
+        TitleCy = titleCy;
+        DescriptionEn = descriptionEn;
+        DescriptionCy = descriptionCy;
+        Notes = notes;
+    }
 }
---
+++ MargamParkArchives.Core/Entities/ArtefactEntity/ArtefactContentRules.cs
@@ -0,0 +1,25 @@
+﻿using MargamParkArchives.Core.Entities.Validation;
+
+namespace MargamParkArchives.Core.Entities.ArtefactEntity;
+
+public static class ArtefactContentRules
+{
+    public const int TitleMaxLength = 255;
+    public const int DescriptionMaxLength = 500;
+    public const int NotesMaxLength = 1000;
+
+    public static bool IsValidTitle(string? title, string propertyName, out string error)
+    {
+        return StringLengthHelper.ValidateNotTooLong(title, TitleMaxLength, propertyName, out error);
+    }
+
+    public static bool IsValidDescription(string? description, string propertyName, out string error)
+    {
+        return StringLengthHelper.ValidateNotTooLong(description, DescriptionMaxLength, propertyName, out error);
+    }
+
+    public static bool IsValidNotes(string? notes, string propertyName, out string error)
+    {
+        return StringLengthHelper.ValidateNotTooLong(notes, NotesMaxLength, propertyName, out error);
+    }
+}
