93bc5c3e4162c59d2305b0f3ea8085bc96dde311
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 09:46:58 +0000

    Move category validation rules to separate class

--- MargamParkArchives.Core/Entities/Category.cs
+++ MargamParkArchives.Core/Entities/Category.cs
@@ -4,9 +4,6 @@ namespace MargamParkArchives.Core.Entities;
 
 public class Category
 {
-    private const int IdMaxLength = 2;
-    private const int NameMaxLength = 50;
-
     public string Id { get; }
     public string Name { get; }
     public DateTime? DateCreated { get; }
@@ -14,23 +11,9 @@ public class Category
 
     public Category(string id, string name, DateTime? dateCreated = null, DateTime? dateModified = null)
     {
-        if (id.Length == 0)
+        if (!CategoryRules.IsValidId(id, out string error))
         {
-            throw new ArgumentException(string.Format(ValidationMessages.ValueEmptyMessage, nameof(Id)));
-        }
-        else if (id.Length > IdMaxLength)
-        {
-            throw new ArgumentException(string.Format(ValidationMessages.ValueTooLongMessage, nameof(Id),
-                IdMaxLength));
-        }
-        if (name.Length == 0)
-        {
-            throw new ArgumentException(string.Format(ValidationMessages.ValueEmptyMessage, nameof(Name)));
-        }
-        else if (name.Length > NameMaxLength)
-        {
-            throw new ArgumentException(string.Format(ValidationMessages.ValueTooLongMessage, nameof(Name),
-                NameMaxLength));
+            throw new ArgumentException(error);
         }
 
         Id = id;
---
+++ MargamParkArchives.Core/Entities/Validation/CategoryRules.cs
@@ -0,0 +1,17 @@
+﻿namespace MargamParkArchives.Core.Entities.Validation;
+
+public static class CategoryRules
+{
+    public const int IdMaxLength = 2;
+    public const int NameMaxLength = 50;
+
+    public static bool IsValidId(string id, out string error)
+    {
+        return StringLengthHelper.ValidateNotEmptyOrTooLong(id, IdMaxLength, nameof(id), out error);
+    }
+
+    public static bool IsValidName(string name, out string error)
+    {
+        return StringLengthHelper.ValidateNotEmptyOrTooLong(name, NameMaxLength, nameof(name), out error);
+    }
+}
