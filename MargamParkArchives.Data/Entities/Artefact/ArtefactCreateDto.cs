779dc02258be20aa9f52a44808991a1ae833e099
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 00:24:03 +0000

    Create artefact DTOs

---
+++ MargamParkArchives.Data/Entities/Artefact/ArtefactCreateDto.cs
@@ -0,0 +1,26 @@
+﻿namespace MargamParkArchives.Data.Entities.Artefact;
+
+internal record ArtefactCreateDto
+{
+    internal required string IdentifierGroupId { get; init; }
+    // identifier_number and identifier_key are automatically generated
+    internal string? FilePath { get; init; }
+    internal string? ParentId { get; init; }
+    internal string? Notes { get; init; }
+    internal string? TitleEn { get; init; }
+    internal string? TitleCy { get; init; }
+    internal string? DescriptionEn { get; init; }
+    internal string? DescriptionCy { get; init; }
+    internal string? CategoryId { get; init; }
+    internal string? TagsCy { get; init; }
+    internal string? CultureTagEn { get; init; }
+    internal int? PeriodId { get; init; }
+    internal int? CreatorId { get; init; }
+    internal string? LocationCoverage { get; init; }
+    internal string? RightType1 { get; init; }
+    internal string? RightHolder1En { get; init; }
+    internal string? RightHolder1Cy { get; init; }
+    internal bool? VisualArtefact { get; init; }
+    internal int? GeneralLocationId { get; init; }
+    internal int? SpecificLocationId { get; init; }
+}
--- MargamParkArchives.Data/Entities/Artefact/ArtefactDto.cs
+++ MargamParkArchives.Data/Entities/Artefact/ArtefactDto.cs
@@ -1,4 +1,4 @@
-﻿namespace MargamParkArchives.Data.Entities;
+﻿namespace MargamParkArchives.Data.Entities.Artefact;
 
 internal record ArtefactDto
 {
