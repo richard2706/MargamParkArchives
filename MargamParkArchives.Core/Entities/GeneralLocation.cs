a02252e8cb154428d858614efe40dd622e1ce9c8
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 00:23:31 +0000

    Create core model for general location

---
+++ MargamParkArchives.Core/Entities/GeneralLocation.cs
@@ -0,0 +1,31 @@
+﻿using MargamParkArchives.Core.Entities.Validation;
+
+namespace MargamParkArchives.Core.Entities;
+
+public class GeneralLocation
+{
+    private const int NameMaxLength = 255;
+
+    public int Id { get; }
+    public string Name { get; }
+    public DateTime? DateCreated { get; }
+    public DateTime? DateModified { get; }
+
+    public GeneralLocation(int id, string name, DateTime? dateCreated = null, DateTime? dateModified = null)
+    {
+        if (name.Length == 0)
+        {
+            throw new ArgumentException(string.Format(ValidationMessages.ValueEmptyMessage, nameof(Name)));
+        }
+        else if (name.Length > NameMaxLength)
+        {
+            throw new ArgumentException(string.Format(ValidationMessages.ValueTooLongMessage, nameof(Name),
+                NameMaxLength));
+        }
+
+        Id = id;
+        Name = name;
+        DateCreated = dateCreated;
+        DateModified = dateModified;
+    }
+}
