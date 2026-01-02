b5aeb1671e4f943abd929159f15ab1db3c78e504
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 19:36:48 +0000

    Test period core model

---
+++ MargamParkArchives.Core.Tests/Entities/PeriodTests.cs
@@ -0,0 +1,36 @@
+﻿using MargamParkArchives.Core.Entities.PeriodEntity;
+
+namespace MargamParkArchives.Core.Tests.Entities;
+
+public class PeriodTests
+{
+    // Valid construction
+    [Fact]
+    public void Constructor_WithValidData_CreatesObject()
+    {
+        Period actual = new(1, "abc");
+        Assert.NotNull(actual);
+    }
+
+    [Fact]
+    public void Constructor_WithMaxLengthDates_CreatesObject()
+    {
+        string maxLengthDates = new('a', PeriodRules.DatesMaxLength);
+        Period actual = new(1, maxLengthDates);
+        Assert.NotNull(actual);
+    }
+
+    // Invalid construction
+    [Fact]
+    public void Constructor_WithEmptyDates_ThrowsException()
+    {
+        Assert.ThrowsAny<ArgumentException>(() => new Period(1, ""));
+    }
+
+    [Fact]
+    public void Constructor_WithTooLongDates_ThrowsException()
+    {
+        string tooLongDates = new('a', PeriodRules.DatesMaxLength + 1);
+        Assert.ThrowsAny<ArgumentException>(() => new Period(1, tooLongDates));
+    }
+}
