fd904e37f71c44eec817883e843616f78f407546
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 02:05:02 +0000

    Move specific location validation rules to separate class

---
+++ MargamParkArchives.Core/Entities/Validation/SpecificLocationRules.cs
@@ -0,0 +1,11 @@
+﻿namespace MargamParkArchives.Core.Entities.Validation;
+
+public static class SpecificLocationRules
+{
+    private const int SummaryMaxLength = 255;
+
+    public static bool IsValidSummary(string summary, out string error)
+    {
+        return StringLengthHelper.ValidateStringNotEmptyOrTooLong(summary, SummaryMaxLength, out error);
+    }
+}
---
+++ MargamParkArchives.Core/Entities/Validation/StringLengthHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace MargamParkArchives.Core.Entities.Validation;
+
+internal static class StringLengthHelper
+{
+    /// <summary>
+    /// Returns true if the specified string is not empty and does not exceed the maximum length specified.
+    /// </summary>
+    /// <remarks>This method does not check for null values. Callers should ensure that the input string is
+    /// not null before invoking this method.</remarks>
+    /// <param name="value">The string value to validate. Cannot be null.</param>
+    /// <param name="maxLength">The maximum number of characters allowed in the string. Must be greater than zero.</param>
+    /// <param name="error">When this method returns, contains an error message if validation fails; otherwise, null.</param>
+    /// <returns>true if the string is not empty and its length does not exceed the specified maximum; otherwise, false.</returns>
+    internal static bool ValidateStringNotEmptyOrTooLong(string value, int maxLength, out string error)
+    {
+        if (value.Length == 0)
+        {
+            error = ValidationMessages.ValueEmptyMessage;
+            return false;
+        }
+        else if (value.Length > maxLength)
+        {
+            error = ValidationMessages.ValueTooLongMessage;
+            return false;
+        }
+        else
+        {
+            error = "";
+            return true;
+        }
+    }
+}
