5952338fc6b27d1e56167864d2b3e350f87ad8e5
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 17:15:48 +0000

    Refactor identifer key builder test class

--- MargamParkArchives.Core.Tests/Entities/ValidationHelpers/IdentifierKeyBuilderTests.cs
+++ MargamParkArchives.Core.Tests/Entities/ValidationHelpers/IdentifierKeyBuilderTests.cs
@@ -1,18 +1,17 @@
 using MargamParkArchives.Core.Entities.ValidationHelpers;
 
-namespace MargamParkArchives.Core.Tests.Entities.Validation
+namespace MargamParkArchives.Core.Tests.Entities.ValidationHelpers;
+
+public class IdentifierKeyBuilderTests
 {
-    public class IdentifierKeyBuilderTests
+    [Theory]
+    [InlineData("CIS-000001", "CIS", 1)]
+    [InlineData("CIS-201984", "CIS", 201984)]
+    [InlineData("WW2-000001", "WW2", 1)]
+    public void BuildsCorrectIdentiferKey_FromValidIdAndNumber(string expectedIdentifierKey,
+        string identiferGroupId, int identiferNumber)
     {
-        [Theory]
-        [InlineData("CIS-000001", "CIS", 1)]
-        [InlineData("CIS-201984", "CIS", 201984)]
-        [InlineData("WW2-000001", "WW2", 1)]
-        public void BuildsCorrectIdentiferKey_FromValidIdAndNumber(string expectedIdentifierKey,
-            string identiferGroupId, int identiferNumber)
-        {
-            string actualIdentifierKey = IdentifierKeyBuilder.Build(identiferGroupId, identiferNumber);
-            Assert.Equal(expectedIdentifierKey, actualIdentifierKey);
-        }
+        string actualIdentifierKey = IdentifierKeyBuilder.Build(identiferGroupId, identiferNumber);
+        Assert.Equal(expectedIdentifierKey, actualIdentifierKey);
     }
 }
