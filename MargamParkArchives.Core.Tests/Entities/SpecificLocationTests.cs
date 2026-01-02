42b74d088ef4b5d32cacee1e2444656097af3079
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 19:13:42 +0000

    Test specific location core model

---
+++ MargamParkArchives.Core.Tests/Entities/SpecificLocationTests.cs
@@ -0,0 +1,36 @@
+﻿using MargamParkArchives.Core.Entities.SpecificLocationEntity;
+
+namespace MargamParkArchives.Core.Tests.Entities;
+
+public class SpecificLocationTests
+{
+    // Valid construction
+    [Fact]
+    public void Constructor_WithValidData_CreatesObject()
+    {
+        SpecificLocation actual = new(1, "abc");
+        Assert.NotNull(actual);
+    }
+
+    [Fact]
+    public void Constructor_WithMaxLengthSummary_CreatesObject()
+    {
+        string maxLengthSummary = new('a', SpecificLocationRules.SummaryMaxLength);
+        SpecificLocation actual = new(1, maxLengthSummary);
+        Assert.NotNull(actual);
+    }
+
+    // Invalid construction
+    [Fact]
+    public void Constructor_WithEmptySummary_ThrowsException()
+    {
+        Assert.ThrowsAny<ArgumentException>(() => new SpecificLocation(1, ""));
+    }
+
+    [Fact]
+    public void Constructor_WithTooLongSummary_ThrowsException()
+    {
+        string longSummary = new('a', SpecificLocationRules.SummaryMaxLength + 1);
+        Assert.ThrowsAny<ArgumentException>(() => new SpecificLocation(1, longSummary));
+    }
+}
