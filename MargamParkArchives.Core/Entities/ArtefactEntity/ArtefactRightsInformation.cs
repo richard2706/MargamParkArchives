b2ef745a2d00285981bdb364913a0d05174bb5f1
Richard Wakeham <rwakeham27@outlook.com>
2026-01-02 11:33:18 +0000

    Validate artefact rights information properties

--- MargamParkArchives.Core/Entities/ArtefactEntity/ArtefactRightsInformation.cs
+++ MargamParkArchives.Core/Entities/ArtefactEntity/ArtefactRightsInformation.cs
@@ -5,4 +5,24 @@ public class ArtefactRightsInformation
     public string? RightType1 { get; }
     public string? RightHolder1En { get; }
     public string? RightHolder1Cy { get; }
+
+    public ArtefactRightsInformation(string? rightType1, string? rightHolder1En, string? rightHolder1Cy)
+    {
+        if (!ArtefactRightsInformationRules.IsValidRightsInformation(rightType1, nameof(rightType1), out string error))
+        {
+            throw new ArgumentException(error, nameof(rightType1));
+        }
+        else if (!ArtefactRightsInformationRules.IsValidRightsInformation(rightHolder1En, nameof(rightHolder1En), out error))
+        {
+            throw new ArgumentException(error, nameof(rightHolder1En));
+        }
+        else if (!ArtefactRightsInformationRules.IsValidRightsInformation(rightHolder1Cy, nameof(rightHolder1Cy), out error))
+        {
+            throw new ArgumentException(error, nameof(rightHolder1Cy));
+        }
+
+        RightType1 = rightType1;
+        RightHolder1En = rightHolder1En;
+        RightHolder1Cy = rightHolder1Cy;
+    }
 }
---
+++ MargamParkArchives.Core/Entities/ArtefactEntity/ArtefactRightsInformationRules.cs
@@ -0,0 +1,13 @@
+﻿using MargamParkArchives.Core.Entities.Validation;
+
+namespace MargamParkArchives.Core.Entities.ArtefactEntity;
+
+public static class ArtefactRightsInformationRules
+{
+    public const int RightsInformationMaxLength = 255;
+
+    public static bool IsValidRightsInformation(string? information, string propertyName, out string error)
+    {
+        return StringLengthHelper.ValidateNotTooLong(information, RightsInformationMaxLength, propertyName, out error);
+    }
+}
