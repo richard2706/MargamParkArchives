c6b627a7235b7a03a61aa428701f2bd92cda6e56
Richard Wakeham <rwakeham27@outlook.com>
2026-01-03 00:42:29 +0000

    Implement artefact details reader using dapper

---
+++ MargamParkArchives.Core/Database/DataAccess/IArtefactDetailsReader.cs
@@ -0,0 +1,8 @@
+﻿using MargamParkArchives.Core.Entities.ArtefactDetailsReadModel;
+
+namespace MargamParkArchives.Core.Database.DataAccess;
+
+public interface IArtefactDetailsReader
+{
+    public Task<ArtefactDetailsReadModel[]> GetRandomArtefactsAsync(int numArtefacts = 3);
+}
---
+++ MargamParkArchives.Data/MySqlArtefactDetailsReader.cs
@@ -0,0 +1,22 @@
+﻿using MargamParkArchives.Core.Database.DataAccess;
+using MargamParkArchives.Core.Entities.ArtefactDetailsReadModel;
+using MargamParkArchives.Data.Connections;
+using MargamParkArchives.Data.Entities.Artefact;
+using static MargamParkArchives.Data.DatabaseConstants;
+
+namespace MargamParkArchives.Data;
+
+public class MySqlArtefactDetailsReader(IMySqlDataAccess dataAccess) : IArtefactDetailsReader
+{
+    private readonly IMySqlDataAccess _dataAccess = dataAccess;
+
+    private const string GetRandomArtefactsQuery = "select * from @Table order by rand() limit @Limit;";
+
+    public async Task<ArtefactDetailsReadModel[]> GetRandomArtefactsAsync(int numArtefacts = 3)
+    {
+        IEnumerable<ArtefactDetailsDto> artefacts = await _dataAccess.GetManyItemsAsync<ArtefactDetailsDto, dynamic>(
+            GetRandomArtefactsQuery,
+            new { Table = ArtefactDetailsViewName, Limit = numArtefacts });
+        return artefacts.Select(dto => dto.ToArtefactDetailsReadModel()).ToArray();
+    }
+}
