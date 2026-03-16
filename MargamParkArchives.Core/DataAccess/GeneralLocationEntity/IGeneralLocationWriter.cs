fde88bcea44f9611276904af099477e2f0a91ad5
Richard Wakeham <rwakeham27@outlook.com>
2026-03-16 19:26:56 +0000

    Implement general location create and update methods
    Note these methods are not tested as previous database methods have been.

---
+++ MargamParkArchives.Core/DataAccess/GeneralLocationEntity/IGeneralLocationWriter.cs
@@ -0,0 +1,18 @@
+﻿namespace MargamParkArchives.Core.DataAccess.GeneralLocationEntity;
+
+public interface IGeneralLocationWriter
+{
+    /// <summary>
+    /// Creates a new general location item in the database asynchronously.
+    /// </summary>
+    /// <param name="generalLocation">Object containing the values for the new general location item.</param>
+    /// <returns>The id of the newly created general location item</returns>
+    public Task<int> CreateGeneralLocationAsync(GeneralLocationCreateDto generalLocation);
+
+    /// <summary>
+    /// Updates an existing general location item in the database asynchronously.
+    /// </summary>
+    /// <param name="generalLocation">Object containing the updated values for the general location.</param>
+    /// <returns>True if the general location was updated successfully or false if the general location was not found.</returns>
+    public Task<bool> UpdateGeneralLocationAsync(GeneralLocationUpdateDto generalLocation);
+}
---
+++ MargamParkArchives.Data/Entities/GeneralLocationEntity/MySqlGeneralLocationWriter.cs
@@ -0,0 +1,38 @@
+﻿using MargamParkArchives.Core.DataAccess.GeneralLocationEntity;
+using MargamParkArchives.Core.Entities.GeneralLocationEntity;
+using MargamParkArchives.Core.Entities.ValidationHelpers;
+using MargamParkArchives.Data.Connections;
+
+namespace MargamParkArchives.Data.Entities.GeneralLocationEntity;
+
+public class MySqlGeneralLocationWriter(IMySqlDataAccess dataAccess) : IGeneralLocationWriter
+{
+    private readonly IMySqlDataAccess _dataAccess = dataAccess;
+
+    private const string InsertCreatorQuery = "insert into {0} (name) values (@Name);";
+    private const string UpdateCreatorQuery = "update {0} set name = @Name where general_location_id = @GeneralLocationId;";
+
+    public async Task<int> CreateGeneralLocationAsync(GeneralLocationCreateDto generalLocation)
+    {
+        bool isNameValid = GeneralLocationValidationRules.IsValidName(generalLocation.Name, nameof(generalLocation.Name), out string nameError);
+        if (!isNameValid)
+        {
+            throw new ValidationException(nameError, nameof(generalLocation.Name));
+        }
+
+        string sqlQuery = string.Format(InsertCreatorQuery, DatabaseConstants.GeneralLocationTableName);
+        return await _dataAccess.InsertAndReturnIdAsync(sqlQuery, generalLocation);
+    }
+
+    public async Task<bool> UpdateGeneralLocationAsync(GeneralLocationUpdateDto generalLocation)
+    {
+        bool isNameValid = GeneralLocationValidationRules.IsValidName(generalLocation.Name, nameof(generalLocation.Name), out string nameError);
+        if (!isNameValid)
+        {
+            throw new ValidationException(nameError, nameof(generalLocation.Name));
+        }
+
+        string sqlQuery = string.Format(UpdateCreatorQuery, DatabaseConstants.GeneralLocationTableName);
+        return await _dataAccess.UpdateAsync<GeneralLocationUpdateDto>(sqlQuery, generalLocation);
+    }
+}
