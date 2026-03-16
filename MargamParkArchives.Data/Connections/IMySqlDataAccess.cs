f4eb52aaa54b51c57c8bb58f01ff8fd65bd43e8e
Richard Wakeham <rwakeham27@outlook.com>
2026-03-16 19:09:57 +0000

    Implement InsertAndReturnIdAsync data access method

--- MargamParkArchives.Data/Connections/IMySqlDataAccess.cs
+++ MargamParkArchives.Data/Connections/IMySqlDataAccess.cs
@@ -2,13 +2,99 @@
 
 public interface IMySqlDataAccess
 {
-    // Read methods
+    #region Read methods
+
+    /// <summary>
+    /// Gets a collection of items of the specified type T from the database asynchronously.
+    /// </summary>
+    /// <typeparam name="T">The type of the items to be returned which should correspond to the database
+    /// entity.</typeparam>
+    /// <param name="sqlQuery">The SQL select query to execute. The entity being selected must correspond to the type
+    /// T of items being returned.</param>
+    /// <returns>A task that represents the asynchronous operation, which will return a collection of items of type T
+    /// returned by the query.</returns>
     public Task<IEnumerable<T>> GetManyItemsAsync<T>(string sqlQuery);
+
+    /// <summary>
+    /// Gets a collection of items of the specified type T from the database asynchronously.
+    /// </summary>
+    /// <typeparam name="T">The type of the items to be returned which should correspond to the database
+    /// entity.</typeparam>
+    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
+    /// <param name="sqlQuery">The SQL select query to execute. The entity being selected must correspond to the type
+    /// T of items being returned.</param>
+    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
+    /// property names in the object should match the parameter names in the query.</param>
+    /// <returns>A task that represents the asynchronous operation, which will return a collection of items of type T
+    /// returned by the query.</returns>
     public Task<IEnumerable<T>> GetManyItemsAsync<T, P>(string sqlQuery, P parameters);
+
+    /// <summary>
+    /// Gets a single item of the specified type T from the database asynchronously. Will return the first item if the
+    /// query returns multiple items or null if the item doesn't exist.
+    /// </summary>
+    /// <typeparam name="T">The type of the item to be returned which should correspond to the database
+    /// entity.</typeparam>
+    /// <param name="sqlQuery">The SQL select query to execute. The entity being selected must correspond to the type
+    /// T of items being returned.</param>
+    /// <returns>A task that represents the asynchronous operation, which will return an item of type T?
+    /// returned by the query, or the first item if the query returns multiple items, or null if the item doesn't
+    /// exist.</returns>
     public Task<T?> GetOneItemAsync<T>(string sqlQuery);
+
+    /// <summary>
+    /// Gets a single item of the specified type T from the database asynchronously. Will return the first item if the
+    /// query returns multiple items or null if the item doesn't exist.
+    /// </summary>
+    /// <typeparam name="T">The type of the item to be returned which should correspond to the database
+    /// entity.</typeparam>
+    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
+    /// <param name="sqlQuery">The SQL select query to execute. The entity being selected must correspond to the type
+    /// T of items being returned.</param>
+    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
+    /// property names in the object should match the parameter names in the query.</param>
+    /// <returns>A task that represents the asynchronous operation, which will return an item of type T?
+    /// returned by the query, or the first item if the query returns multiple items, or null if the item doesn't
+    /// exist.</returns>
     public Task<T?> GetOneItemAsync<T, P>(string sqlQuery, P parameters);
 
-    // Write methods
+    #endregion Read methods
+
+    #region Write methods
+
+    /// <summary>
+    /// Inserts an item into the database asynchronously. Does not return the inserted item's ID. Useful for items
+    /// where the id is not auto-generated.
+    /// </summary>
+    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
+    /// <param name="sqlQuery">The SQL insert query to execute.</param>
+    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
+    /// property names in the object should match the parameter names in the query.</param>
+    /// <returns>True if the item was inserted sucessfully, false otherwise.</returns>
     public Task<bool> InsertAsync<P>(string sqlQuery, P parameters); // For records with user-specified IDs
+
+    /// <summary>
+    /// Inserts an item into the database asynchronously and return the inserted item's ID. Useful for items
+    /// where the id is auto-generated.
+    /// </summary>
+    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
+    /// <param name="sqlQuery">The SQL insert query to execute.</param>
+    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
+    /// property names in the object should match the parameter names in the query.</param>
+    /// <returns>The id of the newly inserted item.</returns>
+    public Task<int> InsertAndReturnIdAsync<P>(string sqlQuery, P parameters);
+
+    /// <summary>
+    /// Updates an item in the database as specified by the sqlQuery and parameters.
+    /// </summary>
+    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
+    /// <param name="sqlQuery">The SQL update query to execute.</param>
+    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
+    /// property names in the object should match the parameter names in the query.</param>
+    /// <returns>True if the item was updated sucessfully, false otherwise.</returns>
     public Task<bool> UpdateAsync<P>(string sqlQuery, P parameters);
+
+    //public Task<bool> DeleteAsync<P>(string sqlQuery, P parameters);
+
+    #endregion Write methods
 }
--- MargamParkArchives.Data/Connections/MySqlDataAccess.cs
+++ MargamParkArchives.Data/Connections/MySqlDataAccess.cs
@@ -11,23 +11,13 @@ public class MySqlDataAccess(IConnectionStringProvider connectionStringProvider)
 
     private readonly IConnectionStringProvider _connectionStringProvider = connectionStringProvider;
 
+    #region Read methods
+
     public async Task<IEnumerable<T>> GetManyItemsAsync<T>(string sqlQuery)
     {
         return await GetManyItemsAsync<T, object>(sqlQuery, new { });
     }
 
-    /// <summary>
-    /// Gets a collection of items of the specified type T from the database asynchronously.
-    /// </summary>
-    /// <typeparam name="T">The type of the items to be returned which should correspond to the database
-    /// entity.</typeparam>
-    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
-    /// <param name="sqlQuery">The SQL select query to execute. The entity being selected must correspond to the type
-    /// T of items being returned.</param>
-    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
-    /// property names in the object should match the parameter names in the query.</param>
-    /// <returns>A task that represents the asynchronous operation, which will return a collection of items of type T
-    /// returned by the query.</returns>
     public async Task<IEnumerable<T>> GetManyItemsAsync<T, P>(string sqlQuery, P parameters)
     {
         string connectionString = await _connectionStringProvider.GetConnectionStringAsync();
@@ -49,20 +39,6 @@ public class MySqlDataAccess(IConnectionStringProvider connectionStringProvider)
         return await GetOneItemAsync<T, object>(sqlQuery, new { });
     }
 
-    /// <summary>
-    /// Gets a single item of the specified type T from the database asynchronously. Will return the first item if the
-    /// query returns multiple items or null if the item doesn't exist.
-    /// </summary>
-    /// <typeparam name="T">The type of the item to be returned which should correspond to the database
-    /// entity.</typeparam>
-    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
-    /// <param name="sqlQuery">The SQL select query to execute. The entity being selected must correspond to the type
-    /// T of items being returned.</param>
-    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
-    /// property names in the object should match the parameter names in the query.</param>
-    /// <returns>A task that represents the asynchronous operation, which will return an item of type T?
-    /// returned by the query, or the first item if the query returns multiple items, or null if the item doesn't
-    /// exist.</returns>
     public async Task<T?> GetOneItemAsync<T, P>(string sqlQuery, P parameters)
     {
         string connectionString = await _connectionStringProvider.GetConnectionStringAsync();
@@ -79,15 +55,10 @@ public class MySqlDataAccess(IConnectionStringProvider connectionStringProvider)
         return item;
     }
 
-    /// <summary>
-    /// Inserts an item into the database asynchronously. Does not return the inserted item's ID. Useful for items
-    /// where the id is not auto-generated.
-    /// </summary>
-    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
-    /// <param name="sqlQuery">The SQL insert query to execute.</param>
-    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
-    /// property names in the object should match the parameter names in the query.</param>
-    /// <returns>True if the item was inserted sucessfully, false otherwise.</returns>
+    #endregion Read methods
+
+    #region Write methods
+
     public async Task<bool> InsertAsync<P>(string sqlQuery, P parameters)
     {
         string connectionString = await _connectionStringProvider.GetConnectionStringAsync();
@@ -104,14 +75,29 @@ public class MySqlDataAccess(IConnectionStringProvider connectionStringProvider)
         return rowsAffected > 0;
     }
 
-    /// <summary>
-    /// Updates an item in the database as specified by the sqlQuery and parameters.
-    /// </summary>
-    /// <typeparam name="P">The type of the parameters object used to supply values for the SQL query.</typeparam>
-    /// <param name="sqlQuery">The SQL update query to execute.</param>
-    /// <param name="parameters">An object containing the parameter values to be used with the SQL query. The
-    /// property names in the object should match the parameter names in the query.</param>
-    /// <returns>True if the item was updated sucessfully, false otherwise.</returns>
+    public async Task<int> InsertAndReturnIdAsync<P>(string sqlQuery, P parameters)
+    {
+        string connectionString = await _connectionStringProvider.GetConnectionStringAsync();
+        using MySqlConnection connection = new(connectionString);
+        int rowsAffected = 0;
+        try
+        {
+            rowsAffected = await connection.ExecuteAsync(sqlQuery, parameters);
+
+        }
+        catch (MySqlException ex)
+        {
+            throw ex.SqlState == InvalidAuthSqlState ? new DatabasePasswordInvalidException(ex.Message) : ex;
+        }
+
+        if (rowsAffected == 0)
+        {
+            throw new DatabaseException("Insert operation failed.");
+        }
+
+        return connection.ExecuteScalar<int>("SELECT LAST_INSERT_ID();");
+    }
+
     public async Task<bool> UpdateAsync<P>(string sqlQuery, P parameters)
     {
         string connectionString = await _connectionStringProvider.GetConnectionStringAsync();
@@ -127,4 +113,6 @@ public class MySqlDataAccess(IConnectionStringProvider connectionStringProvider)
         }
         return rowsAffected > 0;
     }
+
+    #endregion Write methods
 }
