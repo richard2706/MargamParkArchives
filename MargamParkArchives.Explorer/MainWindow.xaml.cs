0ada2ba41207d7372c244607606e79283111d282
Richard Wakeham <rwakeham27@outlook.com>
2026-06-06 18:43:30 +0100

    Set up MainPage and ArtefactSerachPage structure

--- MargamParkArchives.Explorer/MainWindow.xaml.cs
+++ MargamParkArchives.Explorer/MainWindow.xaml.cs
@@ -21,8 +21,7 @@ public sealed partial class MainWindow : Window
     // Services
     private readonly IArtefactDetailsReader _artefactReader;
     private readonly PasswordDialogService _passwordDialogService;
-
-    private ErrorDialogService? _databaseErrorDialogService;
+    private readonly ErrorDialogService? _databaseErrorDialogService;
 
     public MainWindow(IArtefactDetailsReader artefactReader, PasswordDialogService passwordDialogService)
     {
@@ -30,67 +29,65 @@ public sealed partial class MainWindow : Window
         _artefactReader = artefactReader;
         _passwordDialogService = passwordDialogService;
 
-        LoadRandomArtefacts();
+        this.ExtendsContentIntoTitleBar = true;
+        this.SetTitleBar(this.AppTitleBar);
+        //LoadRandomArtefacts();
     }
 
     // Earliest point where XamlRoot is available
-    private async void RootGrid_Loaded(object sender, RoutedEventArgs e)
-    {
-        _uiLoaded = true;
-        if (_showPasswordDialog)
-        {
-            await ShowPasswordDialogThenReloadAsync();
-        }
-    }
+    //private async void RootGrid_Loaded(object sender, RoutedEventArgs e)
+    //{
+    //    _uiLoaded = true;
+    //    if (_showPasswordDialog)
+    //    {
+    //        await ShowPasswordDialogThenReloadAsync();
+    //    }
+    //}
 
-    private async void LoadRandomArtefacts()
-    {
-        try
-        {
-            _artefacts = await _artefactReader.GetRandomArtefactsAsync();
-        }
-        catch (Exception ex) when (ex is PasswordFileMissingException or DatabasePasswordInvalidException)
-        {
-            await ShowPasswordDialogThenReloadAsync();
-            return;
-        }
-        catch (Exception ex)
-        {
-            // Display Error Message info bar
-            Debug.WriteLine(ex.Message);
-            DatabaseConnectionFailedInfoBar.IsOpen = true;
+    //private async void LoadRandomArtefacts()
+    //{
+    //    try
+    //    {
+    //        _artefacts = await _artefactReader.GetRandomArtefactsAsync();
+    //    }
+    //    catch (Exception ex) when (ex is PasswordFileMissingException or DatabasePasswordInvalidException)
+    //    {
+    //        await ShowPasswordDialogThenReloadAsync();
+    //        return;
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        // Display Error Message info bar
+    //        Debug.WriteLine(ex.Message);
+    //        DatabaseConnectionFailedInfoBar.IsOpen = true;
 
-            string errorTitle = ex.GetType().Name;
-            string errorDetails = $"{ex.Message}\nStack Trace: {ex.StackTrace}\nSource: {ex.Source}\nInnerException: {ex.InnerException}";
-            _databaseErrorDialogService = new ErrorDialogService(errorTitle, errorDetails);
+    //        string errorTitle = ex.GetType().Name;
+    //        string errorDetails = $"{ex.Message}\nStack Trace: {ex.StackTrace}\nSource: {ex.Source}\nInnerException: {ex.InnerException}";
+    //        _databaseErrorDialogService = new ErrorDialogService(errorTitle, errorDetails);
 
-            return;
-        }
-
-        // Display success message
-        ArtefactsLoadedInfoBar.Message = $"{_artefacts.Length} artefacts loaded.";
-        ArtefactsLoadedInfoBar.IsOpen = true;
-    }
+    //        return;
+    //    }
+    //}
 
     /// <summary>
     /// Attempts to show the password dialog then reload the artefacts if the dialog returns successfully. If the UI is
     /// not yet loaded, sets the _showPasswordDialog flag to true.
     /// </summary>
     /// <returns></returns>
-    private async Task ShowPasswordDialogThenReloadAsync()
-    {
-        if (_uiLoaded && await _passwordDialogService.ShowDialog(Content.XamlRoot))
-        {
-            LoadRandomArtefacts();
-        }
-        else
-        {
-            _showPasswordDialog = true;
-        }
-    }
+    //private async Task ShowPasswordDialogThenReloadAsync()
+    //{
+    //    if (_uiLoaded && await _passwordDialogService.ShowDialog(Content.XamlRoot))
+    //    {
+    //        LoadRandomArtefacts();
+    //    }
+    //    else
+    //    {
+    //        _showPasswordDialog = true;
+    //    }
+    //}
 
-    private void ViewErrorButton_Click(object sender, RoutedEventArgs e)
-    {
-        _databaseErrorDialogService?.ShowDialog(Content.XamlRoot);
-    }
+    //private void ViewErrorButton_Click(object sender, RoutedEventArgs e)
+    //{
+    //    _databaseErrorDialogService?.ShowDialog(Content.XamlRoot);
+    //}
 }
