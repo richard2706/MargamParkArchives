6b6f4dc551ff9600f14ce1b94abf3b47264a43de
Richard Wakeham <rwakeham27@outlook.com>
2026-07-18 12:16:38 +0100

    Create row classes for TableView
    - Accepting the corresponding query result as a consturctor parameter

---
+++ MargamParkArchives.AdminConsole/AdminArtefactRow.cs
@@ -0,0 +1,37 @@
+﻿using MargamParkArchives.Data.Entities.ArtefactEntity;
+using MargamParkArchives.Windows.UI.TableRows;
+using System;
+
+namespace MargamParkArchives.AdminConsole;
+
+internal class AdminArtefactRow : ArtefactRowBase
+{
+    private AdminArtefactRowQueryResult RowQueryResult { get; init; }
+
+    // Base class properties
+    public override string? IdentifierKey => this.RowQueryResult.identifier_key;
+    public override string? IdentifierGroupName => this.RowQueryResult.identifier_group_name;
+    public override string? FilePath => this.RowQueryResult.file_path;
+    public override DateTime? DateCreated => this.RowQueryResult.date_created;
+    public override string? DescriptionEn => this.RowQueryResult.description_en;
+    public override bool? VisualArtefact => this.RowQueryResult.visual_artefact;
+    public override string? CategoryName => this.RowQueryResult.category_name;
+    public override string? CreatorName => this.RowQueryResult.creator_name;
+    public override string? PeriodDates => this.RowQueryResult.period_dates;
+
+    // Admin Console specific properties
+    public DateTime? DateModified => this.RowQueryResult.date_modified;
+    public string? GeneralLocationName => this.RowQueryResult.general_location_name;
+    public string? SpecificLocationSummary => this.RowQueryResult.specific_location_summary;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="queryResult"></param>
+    public AdminArtefactRow(AdminArtefactRowQueryResult queryResult)
+    {
+        ArgumentNullException.ThrowIfNull(queryResult);
+
+        this.RowQueryResult = queryResult;
+    }
+}
---
+++ MargamParkArchives.Windows.UI/TableRows/ArtefactRowBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MargamParkArchives.Windows.UI.TableRows;
+
+/// <summary>
+/// Specifies the columns displayed at minimum in a TableView row for an Artefact
+/// </summary>
+/// <remarks>
+/// Nullable properties specified here because we should account for missing values when displaying them (e.g. value
+/// may be null if the database was manually edited)
+/// </remarks>
+public abstract class ArtefactRowBase
+{
+    public abstract string? IdentifierKey { get; }
+    public abstract string? IdentifierGroupName { get; }
+    public abstract string? FilePath { get; }
+    public abstract DateTime? DateCreated { get; }
+    public abstract string? DescriptionEn { get; }
+    public abstract bool? VisualArtefact { get; }
+    public abstract string? CategoryName { get; }
+    public abstract string? CreatorName { get; }
+    public abstract string? PeriodDates { get; }
+}
